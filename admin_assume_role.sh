ee750dd4682e77382b8f635f0f8db7eb587f5508
Dmitry Stolyarov <leshiy74@yandex.ru>
2023-09-14 09:55:18 +0300

    add new very cool script admin_assume_role

---
+++ admin_assume_role.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# "for use this script you need to install jq 'sudo apt install jq -y'"
+echo ""
+read -p "Enter your profile " aws_profile
+read -p "Enter your mfa name " mfa_name
+read -p "Enter your mfa token-code " mfa_token_code
+
+#get credentials and write them to variables
+credentials=$(aws sts assume-role --role-arn arn:aws:iam::546240550610:role/Admins --role-session-name tf --serial-number arn:aws:iam::546240550610:mfa/$mfa_name --token-code $mfa_token_code --profile $aws_profile)
+ACCESS_KEY_ID=$(echo $credentials | jq --raw-output '.Credentials.AccessKeyId')
+SECRET_ACCESS_KEY=$(echo $credentials | jq --raw-output '.Credentials.SecretAccessKey')
+SESSION_TOKEN=$(echo $credentials | jq '.Credentials.SessionToken')
+echo ""
+echo "We create new aws profile 'bpzb' and write it to ~/.aws/credentials"
+echo ""
+echo "To use new profile you need use aws with '--profile bpzb'"
+echo "For example: aws --profile bpzb sts get-caller-identity"
+echo ""
+echo "To use new profile in terraform you need write 'profile = "bpzb"' in 'provider "aws"' to ../main.tf"
+echo ""
+# check exist name
+if grep -Fxq "[bpzb]" ~/.aws/credentials
+then
+#if exist, delete it
+	start_line=$(awk '/\[bpzb\]/{print NR}' ~/.aws/credentials)
+	end_line=$(($start_line+3))
+	sed -i "$start_line,$end_line"'d' ~/.aws/credentials
+fi
+# creating template for writing to ~/.aws/credentials
+aws_credentials=("[bpzb]")
+aws_credentials+=("aws_access_key_id = $ACCESS_KEY_ID")
+aws_credentials+=("aws_secret_access_key = $SECRET_ACCESS_KEY")
+aws_credentials+=("aws_session_token = $SESSION_TOKEN")
+# write template to  ~/.aws/credentials
+for i in "${aws_credentials[@]}"; do echo "$i" >> ~/.aws/credentials; done
+

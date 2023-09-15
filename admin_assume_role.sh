9b42523da1729a539fdc948e76df4ee54c4f70ca
Dmitry Stolyarov <leshiy74@yandex.ru>
2023-09-15 13:05:10 +0300

    fix error 'InvalidClientTokenId' and fix text output

--- admin_assume_role.sh
+++ admin_assume_role.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # "for use this script you need to install jq 'sudo apt install jq -y'"
-# get account id
-account_id=$(aws sts get-caller-identity | jq --raw-output '.Account')
 # get user data
 echo ""
-read -p "Enter your profile " aws_profile
+read -p "Enter your profile " aws_profile 
 read -p "Enter your mfa name " mfa_name
+# get account id
+account_id=$(aws --profile $aws_profile sts get-caller-identity | jq --raw-output '.Account')
+# get mfa token
 read -p "Enter your mfa token-code " mfa_token_code
 
 #get credentials and write them to variables
@@ -13,12 +14,25 @@ credentials=$(aws sts assume-role --role-arn arn:aws:iam::$account_id:role/Admin
 ACCESS_KEY_ID=$(echo $credentials | jq --raw-output '.Credentials.AccessKeyId')
 SECRET_ACCESS_KEY=$(echo $credentials | jq --raw-output '.Credentials.SecretAccessKey')
 SESSION_TOKEN=$(echo $credentials | jq '.Credentials.SessionToken')
-echo ""
-echo "We create new aws profile 'bpzb' and write it to ~/.aws/credentials"
-echo ""
-echo "To use new profile you need use aws with '--profile bpzb'"
-echo "For example: aws --profile bpzb sts get-caller-identity"
-echo ""
+# print this text only if the user bpzb not set
+if [[ -v AWS_PROFILE ]]; then
+	if [[ $AWS_PROFILE != "bpzb" ]]; then
+		echo ""
+		echo "We create new aws profile 'bpzb' and write it to ~/.aws/credentials"
+		echo ""
+		echo "To use new profile you need use aws with '--profile bpzb'"
+		echo "For example: aws --profile bpzb sts get-caller-identity"
+		echo ""
+	fi
+else
+	echo ""
+	echo "We create new aws profile 'bpzb' and write it to ~/.aws/credentials"
+	echo ""
+	echo "To use new profile you need use aws with '--profile bpzb'"
+	echo "For example: aws --profile bpzb sts get-caller-identity"
+	echo ""
+fi
+
 # check exist name
 if grep -Fxq "[bpzb]" ~/.aws/credentials
 then
@@ -34,8 +48,20 @@ aws_credentials+=("aws_secret_access_key = $SECRET_ACCESS_KEY")
 aws_credentials+=("aws_session_token = $SESSION_TOKEN")
 # write template to  ~/.aws/credentials
 for i in "${aws_credentials[@]}"; do echo "$i" >> ~/.aws/credentials; done
-echo ""
-echo ""
-echo "Please, execute this command: export AWS_PROFILE=bpzb"
-echo ""
-echo ""
+# print this text only if the user bpzb not set
+if [[ -v AWS_PROFILE ]]; then
+	if [[ $AWS_PROFILE != "bpzb" ]]; then
+		echo ""
+		echo ""
+		echo "Please, execute this command: export AWS_PROFILE=bpzb"
+		echo ""
+		echo ""
+	fi
+else
+	echo ""
+	echo ""
+	echo "Please, execute this command: export AWS_PROFILE=bpzb"
+	echo ""
+	echo ""
+fi
+
